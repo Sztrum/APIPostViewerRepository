7a64fde1bfdbfc2db108a8d43ec47384c4428443
Sztrum <konrad.nowicki@onet.eu>
2024-02-02 11:29:16 +0100

    clean code

--- src/js/project.js
+++ src/js/project.js
@@ -2,30 +2,27 @@ let page = 1;
 
 const fetchOptions = {
   cache: 'force-cache',
-}
+};
 
 const fetchData = async (page, pageSize = 2) => {
   try {
     const cachedPosts = localStorage.getItem(`cachePorts-${page}`);
     const cachedUsers = localStorage.getItem(`cacheUsers`);
 
-    if(cachedPosts && cachedUsers){
+    if (cachedPosts && cachedUsers) {
       console.log('Data from local storage.');
       return [JSON.parse(cachedPosts), JSON.parse(cachedUsers)];
     }
 
     const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${pageSize}`, fetchOptions);
     const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users', fetchOptions);
-    // const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
-    // console.log(postsResponse.json());
 
-    if(!postsResponse.ok || !usersResponse.ok){
+    if (!postsResponse.ok || !usersResponse.ok) {
       throw new Error(`Status: Failed to fetch`);
     }
-  
+
     const [posts, users] = await Promise.all([
       postsResponse.json(),
-      // commentsResponse.json(),
       usersResponse.json(),
     ]);
 
@@ -33,15 +30,14 @@ const fetchData = async (page, pageSize = 2) => {
     localStorage.setItem('cacheUsers', JSON.stringify(users));
 
     return [posts, users];
-    
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
+    throw error;
   }
-
 };
 
-const getUserForPost = (userId, users) =>{
-  const user = users.find(user => user.id === userId);
+const getUserForPost = (userId, users) => {
+  const user = users.find((user) => user.id === userId);
   return [user.name, user.email];
 };
 
@@ -61,57 +57,58 @@ const getCommentsForPost = async (postIdva) => {
 
     const comments = await commentsResponse.json();
 
-    const commentData = comments.map(comment => ({
+    const commentData = comments.map((comment) => ({
       email: comment.email,
-      body: comment.body
+      body: comment.body,
     }));
 
     localStorage.setItem(`cachedComments_${postIdva}`, JSON.stringify(commentData));
 
     return commentData;
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
+    throw error;
   }
 };
 
 
 const drawComments = async (CommentsExpanded, commentData) =>{
-    let CommentArray = [];
-    if (CommentsExpanded || commentData.length <= 2) {
-      commentData.forEach(comment => {
-        CommentArray.push(`
-        <div class="d-flex align-items-start mt-3 flex-wrap flex-sm-nowrap">
-              <img width="35" height="35" class="me-2 avatar-sm rounded-circle" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${comment.email}" alt="${comment.email} Avatar">
-              <div class="w-100">
-                  <div class="d-flex justify-content-between">
-                      <h6 class="mt-2 mb-0">${comment.email}</h6>
-                  </div>
-                  <p class="fs-6 mt-3 mb-0 fw-light">
-                      ${comment.body}
-                  </p>
-              </div>
-          </div>
-        `);
-      });
-    }else{
-      for (let i = 0; i < 2; i++) {
-        if (commentData[i]) {
-          CommentArray +=`
-        <div class="d-flex align-items-start mt-3 flex-wrap flex-sm-nowrap">
-              <img width="35" height="35" class="me-2 avatar-sm rounded-circle" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${commentData[i].email}" alt="${commentData[i].email} Avatar">
-              <div class="w-100">
-                  <div class="d-flex justify-content-between">
-                      <h6 class="mt-2 mb-0">${commentData[i].email}</h6>
-                  </div>
-                  <p class="fs-6 mt-3 mb-0 fw-light">
-                      ${commentData[i].body}
-                  </p>
-              </div>
-          </div>
-          `;
-        }
+  let CommentArray = [];
+  if (CommentsExpanded || commentData.length <= 2) {
+    commentData.forEach((comment) => {
+      CommentArray.push(`
+      <div class="d-flex align-items-start mt-3 flex-wrap flex-sm-nowrap">
+            <img width="35" height="35" class="me-2 avatar-sm rounded-circle" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${comment.email}" alt="${comment.email} Avatar">
+            <div class="w-100">
+                <div class="d-flex justify-content-between">
+                    <h6 class="mt-2 mb-0">${comment.email}</h6>
+                </div>
+                <p class="fs-6 mt-3 mb-0 fw-light">
+                    ${comment.body}
+                </p>
+            </div>
+        </div>
+      `);
+    });
+  } else {
+    for (let i = 0; i < 2; i++) {
+      if (commentData[i]) {
+        CommentArray +=`
+      <div class="d-flex align-items-start mt-3 flex-wrap flex-sm-nowrap">
+            <img width="35" height="35" class="me-2 avatar-sm rounded-circle" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${commentData[i].email}" alt="${commentData[i].email} Avatar">
+            <div class="w-100">
+                <div class="d-flex justify-content-between">
+                    <h6 class="mt-2 mb-0">${commentData[i].email}</h6>
+                </div>
+                <p class="fs-6 mt-3 mb-0 fw-light">
+                    ${commentData[i].body}
+                </p>
+            </div>
+        </div>
+        `;
       }
     }
+  }
   return CommentArray;
 };
 
@@ -131,15 +128,15 @@ const displayPosts = async () => {
     // const comments = data[1];
     const users = data[1];
 
-    for (const post of posts){
-      let  PostElement = document.createElement('div');
+    for (const post of posts) {
+      const PostElement = document.createElement('div');
       PostElement.classList.add('post-element');
       PostElement.setAttribute('data-comments-expanded', 'false');
 
       // get comments for posts
-      let CommentsExpanded = false;
+      const CommentsExpanded = false;
       const commentData = await getCommentsForPost(post.id);
-      let displayComments = await drawComments(CommentsExpanded, commentData);
+      const displayComments = await drawComments(CommentsExpanded, commentData);
 
       // Get users for posts and comments
       const [AuthorPost, AuthorEmail] = getUserForPost(post.userId, users);
@@ -151,7 +148,7 @@ const displayPosts = async () => {
           <div class="px-3 pt-4 pb-2">
             <div class="d-flex align-items-center justify-content-between">
               <div class="d-flex align-items-center">
-                <img width="50" height="50" class="me-2 avatar-sm rounded-circle loading="lazy" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${AuthorPost}" alt="${AuthorPost.split(' ')[0]}">
+                <img width="50" height="50" class="me-2 avatar-sm rounded-circle" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=${AuthorPost}" alt="${AuthorPost.split(' ')[0]}">
                 <div>
                   <p class="name mb-0">${AuthorEmail}</p>
                   <h5 class="card-title mb-0">${post.title}</h5>
@@ -187,8 +184,9 @@ const displayPosts = async () => {
       space.setAttribute('style', 'display:none !important');
       postsContainer.setAttribute('style', 'opacity:1');
     };
-  }catch(error){
-    console.log(error.message);
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
 };
 
@@ -201,9 +199,9 @@ const ioOptions = {
 let io;
 
 const createIntersectionObserver = () => {
-  io = new IntersectionObserver(entries => {
-    entries.forEach(entry =>{
-      if(entry.intersectionRatio > 0){
+  io = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+      if (entry.intersectionRatio > 0) {
         page++;
         displayPosts();
       }
@@ -211,36 +209,36 @@ const createIntersectionObserver = () => {
   }, ioOptions);
 };
 
-const observeFooter = () =>{
+const observeFooter = () => {
   const container = document.querySelector('.footer-container');
   io.observe(container);
 };
 
 const expandComments = () => {
-  document.addEventListener('click', async event => {
+  document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('expand-comments')) {
       const postElement = event.target.closest('.post-element');
       if (postElement) {
-        postElement.querySelector('.expand-comments').style.display = "none";
-        let postId = event.target.getAttribute('data-id');
+        postElement.querySelector('.expand-comments').style.display = 'none';
+        const postId = event.target.getAttribute('data-id');
         postElement.setAttribute('data-comments-expanded', 'true');
-        let areCommentsExpanded = postElement.getAttribute('data-comments-expanded') === 'true';
+        const areCommentsExpanded = postElement.getAttribute('data-comments-expanded') === 'true';
 
         try {
           const commentElement = postElement.querySelector('.comments-element');
           const commentData = await getCommentsForPost(postId);
-          
           const ExpandedComments = await drawComments(areCommentsExpanded, commentData);
 
           // Clear existing comments
           commentElement.innerHTML = '';
 
           // Inject the comments into the DOM
-          ExpandedComments.forEach(commentHtml => {
+          ExpandedComments.forEach((commentHtml) => {
             commentElement.innerHTML += commentHtml;
           });
         } catch (error) {
           console.error(error.message);
+          throw error;
         }
       }
     }
@@ -248,13 +246,10 @@ const expandComments = () => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  displayPosts();
-
+  displayPosts()
   // observe ending
-  createIntersectionObserver();
-  observeFooter();
-
-
+  createIntersectionObserver()
+  observeFooter()
   // expand comments
-  expandComments();
+  expandComments()
 });
